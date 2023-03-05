52d7bd6a5b1cfcc56011b8bc15e0a42c86a77dfa
Ariiel08 <ariel_ortega98@hotmail.com>
2023-03-04 20:33:00 -0400

    Confirm and cancel buttons for edit input

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 "use strict";
 
+var tempLabel;
+var tempInput;
+
 function onChecked(e) {
     let isCompleted = e.checked;
 
@@ -8,11 +11,7 @@ function onChecked(e) {
 
     if(isCompleted){
         label.innerHTML = `<del>${label.innerHTML}</del>`;
-
-        console.log(buttonsContainer.firstElementChild);
         buttonsContainer.firstElementChild.classList.add("hidden");
-
-        
     }else{
         let labelValue = label.lastElementChild.innerHTML;
         label.removeChild(label.lastElementChild);
@@ -23,35 +22,74 @@ function onChecked(e) {
 }
 
 function addTask(e){
-    e.addEventListener("keyup", ({key}) => {
-        if(key === "Enter"){
-            
+
+    if(e.tagName.toLowerCase() === "input"){
+        let btn = document.getElementById("addButtonTask");
+
+        e.addEventListener("keyup", ({key}) => {
             if(e.value !== ""){
-                const task = document.createElement("div");
-                const outerDiv = document.createElement("div");
-
-                outerDiv.classList.add("group");
-
-                task.classList.add("flex", "gap-8", "items-center","group-hover:bg-gray-600", "rounded-md", "px-2", "py-1", "cursor-pointer");
-
-                let uuid = crypto.randomUUID();
-
-                task.innerHTML = `<input class="cursor-pointer" onclick="onChecked(this)" type="checkbox" id="${uuid}">
-                                    <label class="overflow-hidden break-words w-full cursor-pointer" for="${uuid}">${e.value}</label>
-                                    <div class="buttons-container flex gap-4 invisible group-hover:visible">
-                                        <button type="button" onclick="editTask(this)" title="edit-button" class="hover:text-blue-300 text-xl"><ion-icon name="create-outline"></ion-icon></button>
-                                        <button type="button" onclick="deleteTask(this)" title="remove-button" class="hover:text-red-600 text-lg"><ion-icon name="trash-outline"></ion-icon></button>
-                                    </div>
-                                `
-                outerDiv.appendChild(task);                  
-
-                document.querySelector(".task-container").appendChild(outerDiv);
-
-                e.value = "";
+                btn.disabled = false;
+            }else{
+                btn.disabled = true;
             }
-            
+
+            if(key === "Enter"){
+                if(e.value !== ""){
+                    const task = document.createElement("div");
+                    const outerDiv = document.createElement("div");
+    
+                    outerDiv.classList.add("group");
+    
+                    task.classList.add("flex", "gap-8", "items-center","group-hover:bg-gray-600", "rounded-md", "px-2", "py-1", "cursor-pointer");
+    
+                    let uuid = crypto.randomUUID();
+    
+                    task.innerHTML = `<input class="cursor-pointer" onclick="onChecked(this)" type="checkbox" id="${uuid}">
+                                        <label class="overflow-hidden break-words w-full cursor-pointer" for="${uuid}">${e.value}</label>
+                                        <div class="buttons-container flex gap-4 invisible group-hover:visible">
+                                            <button type="button" onclick="editTask(this)" title="edit-button" class="hover:text-blue-300 text-xl"><ion-icon name="create-outline"></ion-icon></button>
+                                            <button type="button" onclick="deleteTask(this)" title="remove-button" class="hover:text-red-600 text-lg"><ion-icon name="trash-outline"></ion-icon></button>
+                                        </div>
+                                    `
+                    outerDiv.appendChild(task);                  
+    
+                    document.querySelector(".task-container").appendChild(outerDiv);
+    
+                    e.value = "";
+                    btn.disabled = true;
+                }
+                
+            }
+        })
+    }else{
+        let input = document.getElementById("addInputTask");
+
+        if(input.value !== ""){
+            const task = document.createElement("div");
+            const outerDiv = document.createElement("div");
+
+            outerDiv.classList.add("group");
+
+            task.classList.add("flex", "gap-8", "items-center","group-hover:bg-gray-600", "rounded-md", "px-2", "py-1", "cursor-pointer");
+
+            let uuid = crypto.randomUUID();
+
+            task.innerHTML = `<input class="cursor-pointer" onclick="onChecked(this)" type="checkbox" id="${uuid}">
+                                <label class="overflow-hidden break-words w-full cursor-pointer" for="${uuid}">${input.value}</label>
+                                <div class="buttons-container flex gap-4 invisible group-hover:visible">
+                                    <button type="button" onclick="editTask(this)" title="edit-button" class="hover:text-blue-300 text-xl"><ion-icon name="create-outline"></ion-icon></button>
+                                    <button type="button" onclick="deleteTask(this)" title="remove-button" class="hover:text-red-600 text-lg"><ion-icon name="trash-outline"></ion-icon></button>
+                                </div>
+                            `
+            outerDiv.appendChild(task);                  
+
+            document.querySelector(".task-container").appendChild(outerDiv);
+
+            input.value = "";
+            e.disabled = true;
         }
-    })
+    }
+    
 }
 
 function deleteTask(e){
@@ -59,31 +97,68 @@ function deleteTask(e){
 }
 
 function editTask(e){
+    
+    if(e.title === "edit-button"){
+        let buttonsContainer = e.parentElement;
+        let buttonsContainer2 = e.parentElement.parentElement.querySelector(".buttons-container-2");
 
-    let label = e.parentElement.parentElement.getElementsByTagName("label")[0];
+        buttonsContainer.classList.add("hidden");
+        buttonsContainer2.classList.remove("hidden");
 
-    let input = document.createElement("input");
-    input.title = "Task name";
-    input.value = label.textContent;
-    input.classList.add("bg-gray-700", "group-hover:bg-gray-600", "border-b", "focus:outline-none", "w-full");
-    input.type = "text";
+        let label = e.parentElement.parentElement.getElementsByTagName("label")[0];
 
-    let tempLabel = label;
-    label.replaceWith(input);
+        let input = document.createElement("input");
+        input.title = "Task name";
+        input.value = label.textContent;
+        input.classList.add("input-edit", "bg-gray-700", "group-hover:bg-gray-600", "border-b", "focus:outline-none", "w-full");
+        input.type = "text";
 
-    input.focus();
+        tempLabel = label;
+        label.replaceWith(input);
 
-    input.addEventListener("focusout", () => {
-        input.replaceWith(tempLabel);
-    })
+        input.focus();
 
-    input.addEventListener("keyup", ({key}) => {
-        if(key === "Enter"){
-            if(input.value !== ""){
-                tempLabel.textContent = input.value;
-                input.blur();
+        let clicked;
+
+        buttonsContainer2.firstElementChild.addEventListener("mousedown", () => {
+            clicked = true;
+        });
+
+        input.addEventListener("focusout", () => {
+             if(!clicked){
                 input.replaceWith(tempLabel);
+
+                buttonsContainer.classList.remove("hidden");
+                buttonsContainer2.classList.add("hidden");
+             }
+        })
+
+        tempInput = input;
+
+        input.addEventListener("keyup", ({key}) => {
+            if(key === "Enter"){
+                if(input.value !== ""){
+                    tempLabel.textContent = input.value;
+                    input.blur();
+                    input.replaceWith(tempLabel);
+                }
             }
+        });
+    }else{
+
+        let buttonsContainer = e.parentElement.parentElement.querySelector(".buttons-container");
+        let buttonsContainer2 = e.parentElement;
+
+        buttonsContainer.classList.remove("hidden");
+        buttonsContainer2.classList.add("hidden");
+
+        let input = tempInput;
+
+        if(input.value !== ""){
+            tempLabel.textContent = input.value;
+            input.blur();
+            input.replaceWith(tempLabel);
         }
-    });
+    }
+    
 }
---
+++ tailwind.config.cjs
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: {
+    relative: true,
+    files: [
+      "./public/**/*.{html,js}",
+      "./src/**/*.{html,js}",
+      "./src/**/*"
+    ]
+  },
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
